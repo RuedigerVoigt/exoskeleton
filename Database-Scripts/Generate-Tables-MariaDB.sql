288b259a81688635989c06f01a0a7c2c48972306
Rüdiger Voigt <ruedigervoigt@gmx.net>
2019-11-26 11:44:59 +0100

    improve database structure (breaking changes!)

--- Database-Scripts/Generate-Tables-MariaDB.sql
+++ Database-Scripts/Generate-Tables-MariaDB.sql
@@ -1,5 +1,6 @@
 -- ----------------------------------------------------------
 -- EXOSKELETON TABLE STRUCTURE FOR MARIADB
+-- for version 0.6.0 of exoskeleton
 -- © 2019 Rüdiger Voigt
 -- APACHE-2 LICENSE
 --
@@ -7,27 +8,28 @@
 -- exoskeleton framework in MariaDB.
 -- For a documentation of the package, see:
 -- https://github.com/RuedigerVoigt/exoskeleton
--- There you will also find scripts to create this table 
+-- There you will also find scripts to create this table
 -- structure in other dbms.
 --
 -- BEWARE: This should be run in an empty database as otherwise
 -- this script might alter existing tables!
 -- ----------------------------------------------------------
 
-
+-- DON NOT FORGET:
+-- USE nameOfTheDatabase;
 
 -- ----------------------------------------------------------
 -- QUEUE
--- 
+--
 -- Needed by the bot to know what to do next.
 -- ----------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS queue (
     id INT NOT NULL AUTO_INCREMENT
     ,action INT
-    ,url VARCHAR(20148) NOT NULL
+    ,url TEXT NOT NULL
     ,addedToQueue TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    ,causesPermanentError INT NULL
+    ,causesError INT NULL
     ,retries INT NULL
     ,delayUntil TIMESTAMP NULL
     ,PRIMARY KEY(`id`)
@@ -46,57 +48,169 @@ CREATE TABLE IF NOT EXISTS actions (
     ,PRIMARY KEY(`id`)
 ) ENGINE=InnoDB;
 
-INSERT INTO actions (id, description) VALUES 
+INSERT INTO actions (id, description) VALUES
 (1, 'download file to disk'),
 (2, 'save page code into database');
 
+
+ALTER TABLE `queue`
+ADD CONSTRAINT `actions`
+FOREIGN KEY (`action`)
+REFERENCES `actions`(`id`)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
 -- ----------------------------------------------------------
--- PERMANENT ERRORS
+-- ERRORS
 -- ----------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS permanentErrors (
-    id INT NOT NULL AUTO_INCREMENT
+CREATE TABLE IF NOT EXISTS errorType (
+    id INT NOT NULL
     ,short VARCHAR(15)
     ,description VARCHAR(255)
+    ,permanent BOOLEAN
     ,PRIMARY KEY(`id`)
 ) ENGINE=InnoDB;
 
-INSERT INTO permanentErrors (short, description) VALUES 
-('404', 'Server says: File not found'),
-('410', 'Server says: Gone');
+INSERT INTO errorType (id, short, permanent, description) VALUES
+(1, 'malformed url', 1, 'URL is malformed. Missing Schema (http:// or https://) ?'),
+(402, '402', 1, 'Server replied: Payment Required'),
+(403, '403', 1, 'Server replied: Forbidden'),
+(404, '404', 1, 'Server replied: File not found'),
+(405, '405', 1, 'Server replied: Method Not Allowed'),
+(410, '410', 1, 'Server replied: Gone'),
+(414, '414', 1, 'Server replied: URI too long'),
+(429, '429', 0, 'Server replied: Too Many Requests'),
+(451, '451', 1, 'Server replied: Unavailable For Legal Reasons'),
+(500, '500', 0, 'Server replied: Internal Server Error'),
+(503, '503', 0, 'Server replied: Service Unavailable');
+
+ALTER TABLE `queue`
+ADD CONSTRAINT `errors`
+FOREIGN KEY (`causesError`)
+REFERENCES `errorType`(`id`)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
 
 -- ----------------------------------------------------------
 -- DOWNLOADED FILES
 -- ----------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS files (
+CREATE TABLE IF NOT EXISTS fileMaster (
     id INT NOT NULL AUTO_INCREMENT
-    ,downloadDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    ,url VARCHAR(20148) NOT NULL
-    ,storageFolder VARCHAR(255) NULL
-    ,fileName VARCHAR(255) NULL
-    ,size INT NULL
-    ,hashMethod VARCHAR(6) NULL
-    ,hashValue VARCHAR(256) NULL
+    ,initialDownloadDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ,url TEXT NOT NULL
+    ,numVersions_t INT DEFAULT 0
+    -- increment / decrement of version count via trigger in fileVersions
+    -- Default has to be 0 not NULL as increment otherwise does not work
     ,PRIMARY KEY(`id`)
     ) ENGINE=InnoDB;
 
 -- ----------------------------------------------------------
--- 
+-- FILE STORAGE
 -- ----------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS content (
-    id INT NOT NULL AUTO_INCREMENT
-    ,downloadDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    ,url VARCHAR(20148) NOT NULL
-    ,pageContent TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS storageTypes (
+    id INT NOT NULL
+    ,shortName VARCHAR(15)
+    ,fullName VARCHAR(63)
     ,PRIMARY KEY(`id`)
+    ) ENGINE=InnoDB;
+
+INSERT INTO storageTypes (id, shortName, fullName) VALUES
+(1, 'local', 'Local Database'),
+(2, 'local', 'Local Filesystem'),
+(3, 'AWS', 'Amazon Web Services'),
+(4, 'GCP', 'Google Cloud Platform'),
+(5, 'Azure', 'Microsoft Azure'),
+(6, 'Alibaba', 'Alibaba');
+
+CREATE TABLE IF NOT EXISTS fileVersions (
+    id INT NOT NULL AUTO_INCREMENT
+    ,fileID INT NOT NULL
+    ,storageTypeID INT NOT NULL
+    ,fileName VARCHAR(255) NULL
+    ,pathOrBucket VARCHAR(2048) NULL
+    ,versionTimestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ,size INT NULL
+    ,hashMethod VARCHAR(6) NULL
+    ,hashValue VARCHAR(512) NULL
+    ,comment VARCHAR(256) NULL
+    ,PRIMARY KEY(`id`)
+    ,INDEX(`fileID`)
+    ,INDEX(`storageTypeID`)
+    -- No index on fileName as too long for standard index
+    ) ENGINE=InnoDB;
+
+
+ALTER TABLE `fileVersions`
+ADD CONSTRAINT `link from version to master`
+FOREIGN KEY (`fileID`)
+REFERENCES `fileMaster`(`id`)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+ALTER TABLE `fileVersions`
+ADD CONSTRAINT `location-type`
+FOREIGN KEY (`storageTypeID`)
+REFERENCES `storageTypes`(`id`)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+CREATE TRIGGER increment_version_count
+AFTER INSERT ON fileVersions
+FOR EACH ROW
+UPDATE fileMaster
+SET fileMaster.numVersions_t = (fileMaster.numVersions_t + 1)
+WHERE fileMaster.id = NEW.fileID;
+
+DELIMITER //
+
+CREATE TRIGGER decrement_version_count_or_delete_master
+AFTER DELETE ON fileVersions
+FOR EACH ROW
+BEGIN
+
+  UPDATE fileMaster
+  SET fileMaster.numVersions_t = (fileMaster.numVersions_t - 1)
+  WHERE fileMaster.id = OLD.fileID;
+
+  -- in case there is no version left: drop the file from the master table
+  DElETE FROM fileMaster
+  WHERE fileMaster.id = OLD.fileID AND fileMaster.numVersions_t = 0;
+
+END
+
+//
+
+DELIMITER ;
+
+
+-- distinct table in Case somebody uses SELECT *
+CREATE TABLE IF NOT EXISTS fileContent (
+    id INT NOT NULL AUTO_INCREMENT
+    ,versionID INT NOT NULL
+    ,pageContent MEDIUMTEXT NOT NULL
+    ,PRIMARY KEY(`id`)
+    ,INDEX(`versionID`)
 ) ENGINE=InnoDB;
 
+ALTER TABLE `fileContent`
+ADD CONSTRAINT `link from content to version in master`
+FOREIGN KEY (`versionID`)
+REFERENCES `fileVersions`(`id`)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+CREATE TRIGGER removeContent
+AFTER DELETE ON fileContent
+FOR EACH ROW
+DElETE FROM fileVersions
+WHERE fileVersions.id = OLD.versionID;
 
 -- ----------------------------------------------------------
 -- EVENT LOG
--- 
+--
 -- Log important events like start / end of a run, reaching
 -- a milestone, ...
 -- ----------------------------------------------------------
@@ -111,21 +225,19 @@ CREATE TABLE IF NOT EXISTS eventLog (
 
 
 -- ----------------------------------------------------------
--- REFERENTIAL INTEGRITY
+-- VIEWS
+--
+-- For easier access.
 -- ----------------------------------------------------------
 
-ALTER TABLE `queue` 
-ADD CONSTRAINT `errors` 
-FOREIGN KEY (`causesPermanentError`) 
-REFERENCES `permanentErrors`(`id`) 
-ON DELETE RESTRICT  
-ON UPDATE RESTRICT;
-
-ALTER TABLE `queue` 
-ADD CONSTRAINT `actions` 
-FOREIGN KEY (`action`) 
-REFERENCES `actions`(`id`) 
-ON DELETE RESTRICT  
-ON UPDATE RESTRICT;
-
-
+CREATE VIEW v_content AS
+SELECT m.id AS masterFileID
+    ,c.versionID
+    ,v.versionTimestamp
+    ,m.url
+    ,c.pageContent
+FROM fileContent AS c
+LEFT JOIN fileVersions AS v
+ON c.versionID = v.id
+LEFT JOIN fileMaster as m
+on c.versionID = m.id;
