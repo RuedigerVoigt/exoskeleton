7414ec4a1cd5ea53c85dd5eba278ce41c8d5e596
Rüdiger Voigt <ruedigervoigt@gmx.net>
2020-01-12 05:32:30 +0100

    Remove unused parameter; PEP8;

--- exoskeleton/checks.py
+++ exoskeleton/checks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+u""" A range of function to check correctness of parameters """
+
 import hashlib
 import logging
 import re
 
 
-def check_email_format(mailaddress: str,
-                       purpose: str = None) -> str:
+def check_email_format(mailaddress: str) -> str:
     u"""Very basic check if the email address has a valid format
     and returns it as is except if it is obviously false."""
 
@@ -28,16 +29,16 @@ def check_email_format(mailaddress: str,
 
 
 def validate_port(port_number: int,
-                  db: str):
+                  database: str):
     u"""Checks if the port is within range.
     Returns standard port if none is set."""
 
     if port_number is None:
         logging.info('No port number supplied. ' +
                      'Will try standard port instead.')
-        if db == 'mariadb':
+        if database == 'mariadb':
             return 3306
-        elif db == 'postgresql':
+        elif database == 'postgresql':
             return 5432
         else:
             # should not be reached
@@ -68,9 +69,8 @@ def check_hash_algo(hash_method: str):
         logging.debug('Chosen hashing method is available on the system.')
         if hash_method in ('md5', 'sha1'):
             logging.info('Hash method set to %s', hash_method)
-            logging.info('%s is fast, but a weak hashing algorithm. ' +
-                         'Consider using another method if security ' +
-                         'is important.', hash_method)
+            logging.info('%s is fast, but a weak hashing algorithm. ',
+                         hash_method)
             return hash_method
         elif hash_method in ('sha224', 'sha256', 'sha512'):
             logging.info('Hash method set to %s', hash_method)
@@ -97,19 +97,19 @@ def validate_aws_s3_bucket_name(bucket_name: str) -> bool:
         logging.error('Any AWS bucket name has to be at least 3 ' +
                       'characters long.')
         return False
-    elif len(bucket_name) > 63:
+    if len(bucket_name) > 63:
         logging.error('The provided bucket name for AWS exceeds the ' +
                       'maximum length of 63 characters.')
         return False
-    elif not re.match(r"^[a-z0-9\-\.]*$", bucket_name):
+    if not re.match(r"^[a-z0-9\-\.]*$", bucket_name):
         logging.error('The AWS bucket name contains invalid characters.')
         return False
-    elif re.match(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", bucket_name):
+    if re.match(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", bucket_name):
         # Check if the bucket name resembles an IPv4 address.
         # No need to check IPv6 as the colon is not an allowed character.
         logging.error('An AWS must not resemble an IP address.')
         return False
-    elif re.match(r"([a-z0-9][a-z0-9\-]*[a-z0-9]\.)*[a-z0-9][a-z0-9\-]*[a-z0-9]", bucket_name):
+    if re.match(r"([a-z0-9][a-z0-9\-]*[a-z0-9]\.)*[a-z0-9][a-z0-9\-]*[a-z0-9]", bucket_name):
         # must start with a lowercase letter or number
         # Bucket names must be a series of one or more labels.
         # Adjacent labels are separated by a single period (.).
@@ -118,6 +118,6 @@ def validate_aws_s3_bucket_name(bucket_name: str) -> bool:
         # a dot) found here:
         # https://stackoverflow.com/questions/50480924
         return True
-    else:
-        logging.error('Invalid AWS bucket name.')
-        return False
+
+    logging.error('Invalid AWS bucket name.')
+    return False
