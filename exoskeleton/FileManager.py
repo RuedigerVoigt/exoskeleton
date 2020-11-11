bf1972cbc50359b05ae697e86c511f3ec6e3cbd6
Rüdiger Voigt <ruedigervoigt@gmx.net>
2020-11-11 20:35:07 +0100

    Change log messages; Add decorator @staticmethod to 3 methods

--- exoskeleton/FileManager.py
+++ exoskeleton/FileManager.py
@@ -27,6 +27,7 @@ class FileManager:
                  ) -> None:
         self.cur = db_cursor
         self.target_dir = self.__check_target_directory(target_directory)
+        logging.info("Saving files in this directory: %s", self.target_dir)
         self.file_prefix = self.__clean_prefix(filename_prefix)
 
         self.hash_method = 'sha256'
@@ -34,8 +35,8 @@ class FileManager:
             raise ValueError('The hash method SHA256 is not available on ' +
                              'your system.')
 
-    def __check_target_directory(self,
-                                 target_directory: str) -> pathlib.Path:
+    @staticmethod
+    def __check_target_directory(target_directory: str) -> pathlib.Path:
         """Check if a target directory is set to write files to.
            If not fallback to the current working directory.
            If a directory is set, but not accessible, fail early."""
@@ -43,25 +44,20 @@ class FileManager:
         target_dir = pathlib.Path.cwd()
 
         if not target_directory or target_directory.strip() == '':
-            logging.warning("Target directory is not set. " +
-                            "Using the current working directory " +
-                            "%s to store files!",
-                            target_dir)
+            logging.error("Target directory is not set. Using the " +
+                          "current working directory as a fallback!")
         else:
             # Assuming that if a directory was set, it has to be used.
             # Therefore no fallback to the current working directory.
             target_dir = pathlib.Path(target_directory).resolve()
-            if target_dir.is_dir():
-                logging.debug("Set target directory to %s", target_directory)
-            else:
+            if not target_dir.is_dir():
                 raise OSError("Cannot find or access the user " +
                               "supplied target directory! " +
                               "Create this directory or check permissions.")
-
         return target_dir
 
-    def __clean_prefix(self,
-                       file_prefix: str) -> str:
+    @staticmethod
+    def __clean_prefix(file_prefix: str) -> str:
         """Remove whitespace around the filename prefix and check if it is
            not longer than 16 characters."""
         file_prefix = file_prefix.strip()
@@ -96,11 +92,12 @@ class FileManager:
             file_path, self.hash_method)
         return hash_value
 
-    def get_file_size(self,
-                      file_path: pathlib.Path) -> int:
+    @staticmethod
+    def get_file_size(file_path: pathlib.Path) -> int:
         """File size in bytes."""
         try:
             return file_path.stat().st_size
         except Exception:
-            logging.error('Cannot get file size', exc_info=True)
+            logging.error('Cannot get file size of %s',
+                          file_path, exc_info=True)
             raise
