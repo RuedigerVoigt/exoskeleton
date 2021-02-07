95674de6615cc52df4b7a78433fc1b08134f68af
Rüdiger Voigt <ruedigervoigt@gmx.net>
2021-02-07 12:45:54 +0100

    Refactor: introduce inheritance for easier control flow

--- exoskeleton/actions.py
+++ exoskeleton/actions.py
@@ -6,10 +6,10 @@ The class class ExoActions manages actions like downloading
 a file or page content.
 
 Source: https://github.com/RuedigerVoigt/exoskeleton
+(c) 2019-2021 Rüdiger Voigt:
 Released under the Apache License 2.0
 """
 # standard library:
-from collections import Counter
 import logging
 from urllib.parse import urlparse
 
@@ -26,13 +26,10 @@ from exoskeleton import statistics_manager
 from exoskeleton import time_manager
 
 
-class ExoActions:
-    """Manage actions (i.e. interactions with servers)
-       except remote control of the chromium browser. """
+class GetObjectBaseClass:
+    "Base class to get objects."
     # pylint: disable=too-many-instance-attributes
     # pylint: disable=too-many-arguments
-
-    MAX_PATH_LENGTH = 255
 
     # HTTP response codes have to be handeled differently depending on whether
     # they signal a permanent or temporary error. The following lists include
@@ -42,6 +39,193 @@ class ExoActions:
                              407, 410, 451, 501)
     # 429 (Rate Limit) is handeled separately:
     HTTP_TEMP_ERRORS = (408, 500, 502, 503, 504, 509, 529, 598)
+
+    def __init__(
+            self,
+            objects: dict,
+            queue_id: str,
+            url: str,
+            url_hash: str,
+            prettify_html: bool = False):
+        self.cur = objects['db_cursor']
+        self.stats = objects['stats_manager_object']
+        self.file = objects['file_manager_object']
+        self.time = objects['time_manager_object']
+        self.errorhandling = objects['crawling_error_manager_object']
+        self.user_agent = objects['user_agent']
+        self.connection_timeout = objects['connection_timeout']
+        if not isinstance(queue_id, str):
+            raise ValueError('The queue_id must be a string.')
+        self.queue_id = queue_id
+        if not url:
+            raise ValueError('Missing url')
+        self.url = url.strip()
+        if not url_hash:
+            raise ValueError('Missing url_hash')
+        self.url_hash = url_hash
+
+        self.prettify_html = prettify_html
+
+        self.mime_type: str = ''
+
+        self.get_the_object()
+
+    def get_the_object(self) -> None:
+        "Get the object and handle exceptions that might occur"
+        try:
+            response = self.handle_action()
+
+            if response.status_code == 200:
+                content_type = response.headers.get('content-type')
+                if content_type:
+                    self.mime_type = (content_type).split(';')[0]
+                self.store_result(response)
+            elif response.status_code in self.HTTP_PERMANENT_ERRORS:
+                self.errorhandling.mark_permanent_error(
+                    self.queue_id, response.status_code)
+                self.stats.update_host_statistics(self.url, 0, 0, 1, 0)
+            elif response.status_code == 429:
+                # The server tells explicity that the bot hit a rate limit!
+                logging.error('The bot hit a rate limit => increase min_wait.')
+                fqdn = urlparse(self.url).hostname
+                if fqdn:
+                    self.errorhandling.add_rate_limit(fqdn)
+                self.stats.update_host_statistics(self.url, 0, 0, 0, 1)
+            elif response.status_code in self.HTTP_TEMP_ERRORS:
+                logging.info('Temporary error. Adding delay to queue item.')
+                self.errorhandling.add_crawl_delay(
+                    self.queue_id, response.status_code)
+            else:
+                logging.error('Unhandled return code %s', response.status_code)
+                self.stats.update_host_statistics(self.url, 0, 0, 1, 0)
+        except TimeoutError:
+            logging.error('Reached timeout.', exc_info=True)
+            self.errorhandling.add_crawl_delay(self.queue_id, 4)
+            self.stats.update_host_statistics(self.url, 0, 1, 0, 0)
+
+        except ConnectionError:
+            logging.error('Connection Error', exc_info=True)
+            self.stats.update_host_statistics(self.url, 0, 1, 0, 0)
+            raise
+
+        except urllib3.exceptions.NewConnectionError:
+            logging.error('New Connection Error: might be a rate limit',
+                          exc_info=True)
+            self.stats.update_host_statistics(self.url, 0, 0, 0, 1)
+            self.time.increase_wait()
+
+        except requests.exceptions.MissingSchema:
+            logging.error('Missing Schema Exception. Does your URL contain ' +
+                          'the protocol i.e. http:// or https:// ? ' +
+                          'See queue_id = %s', self.queue_id)
+            self.errorhandling.mark_permanent_error(self.queue_id, 1)
+
+        except Exception:
+            logging.error('Unknown exception while trying to download.',
+                          exc_info=True)
+            self.stats.update_host_statistics(self.url, 0, 0, 1, 0)
+            raise
+        self.stats.increment_processed_counter()
+        self.stats.update_host_statistics(self.url, 1, 0, 0, 0)
+
+    def handle_action(self) -> requests.Response:
+        "Do the actual request"
+        raise NotImplementedError('Thou shalt use a derived class')
+
+    def store_result(self,
+                     response: requests.Response,
+                     strip_code: bool = False) -> None:
+        "Write the result to the database"
+        raise NotImplementedError('Thou shalt use a derived class')
+
+
+class GetFile(GetObjectBaseClass):
+    "Download a file"
+    def handle_action(self) -> requests.Response:
+        logging.debug('starting download of queue id %s', self.queue_id)
+        response = requests.get(
+            self.url,
+            headers={"User-agent": self.user_agent},
+            timeout=self.connection_timeout,
+            stream=True)
+        return response
+
+    def store_result(self,
+                     response: requests.Response,
+                     strip_code: bool = False) -> None:
+        extension = userprovided.url.determine_file_extension(
+            self.url, self.mime_type)
+        new_filename = f"{self.file.file_prefix}{self.queue_id}{extension}"
+        file_path = self.file.write_response_to_file(
+            response, new_filename)
+        hash_value = self.file.get_file_hash(file_path)
+
+        try:
+            self.cur.callproc('insert_file_SP',
+                              (self.url, self.url_hash,
+                               self.queue_id,
+                               self.mime_type,
+                               str(self.file.target_dir),
+                               new_filename,
+                               self.file.get_file_size(file_path),
+                               self.file.hash_method,
+                               hash_value, 1))
+        except pymysql.DatabaseError:
+            logging.error('Transaction failed: Could not add ' +
+                          'already downloaded file %s to the database!',
+                          new_filename)
+
+
+class GetContent(GetObjectBaseClass):
+    "Get a page's content including the HTML code"
+    def handle_action(self) -> requests.Response:
+        logging.debug('retrieving content of queue id %s', self.queue_id)
+        response = requests.get(
+            self.url,
+            headers={"User-agent": self.user_agent},
+            timeout=self.connection_timeout,
+            stream=False)
+        return response
+
+    def store_result(self,
+                     response: requests.Response,
+                     strip_code: bool = False) -> None:
+        detected_encoding = str(response.encoding)
+        logging.debug('detected encoding: %s', detected_encoding)
+        page_content = response.text
+        if self.mime_type == 'text/html' and self.prettify_html:
+            page_content = ExoActions.prettify_html(page_content)
+        if strip_code:
+            soup = BeautifulSoup(page_content, 'lxml')
+            page_content = soup.get_text()
+
+        try:
+            # Stored procedure saves the content, transfers the
+            # labels from the queue, and removes the queue item:
+            self.cur.callproc('insert_content_SP',
+                              (self.url, self.url_hash, self.queue_id,
+                               self.mime_type, page_content, 2))
+        except pymysql.DatabaseError:
+            logging.error('Transaction failed: Could not add page code of ' +
+                          'queue item %s to the database!',
+                          self.queue_id, exc_info=True)
+
+
+class GetText(GetContent):
+    "Get the text on a page without the code."
+    def store_result(self,
+                     response: requests.Response,
+                     strip_code: bool = True) -> None:
+        return super().store_result(response, strip_code)
+
+
+class ExoActions:
+    """Manage actions (i.e. interactions with servers)
+       except remote control of the chromium browser. """
+    # pylint: disable=too-many-instance-attributes
+    # pylint: disable=too-many-arguments
+
+    MAX_PATH_LENGTH = 255
 
     def __init__(
             self,
@@ -59,10 +243,20 @@ class ExoActions:
         self.file = file_manager_object
         self.time = time_manager_object
         self.errorhandling = crawling_error_manager_object
-        self.controlled_browser = remote_control_chrome_object
         self.user_agent = user_agent
         self.connection_timeout = connection_timeout
-        self.cnt: Counter = Counter()
+
+        self.controlled_browser = remote_control_chrome_object
+
+        self.objects = {
+            'db_cursor': self.cur,
+            'stats_manager_object': self.stats,
+            'file_manager_object': self.file,
+            'time_manager_object': self.time,
+            'crawling_error_manager_object': self.errorhandling,
+            'user_agent': self.user_agent,
+            'connection_timeout': self.connection_timeout
+        }
 
     def get_object(self,
                    queue_id: str,
@@ -72,147 +266,22 @@ class ExoActions:
                    prettify_html: bool = False) -> None:
         """ Generic function to either download a file or
             store a page's content."""
-        # pylint: disable=too-many-branches
-        if not isinstance(queue_id, str):
-            raise ValueError('The queue_id must be a string.')
-        if action_type not in ('file', 'content', 'text'):
-            raise ValueError('Invalid action')
-        if url == '' or url is None:
-            raise ValueError('Missing url')
-        url = url.strip()
-        if url_hash == '' or url_hash is None:
-            raise ValueError('Missing url_hash')
 
-        if action_type not in ('content', 'text') and prettify_html:
-            logging.error('Wrong action_type: prettify_html ignored.')
-
-        response = requests.Response()
-        try:
-            if action_type == 'file':
-                logging.debug('starting download of queue id %s', queue_id)
-                response = requests.get(
-                    url,
-                    headers={"User-agent": self.user_agent},
-                    timeout=self.connection_timeout,
-                    stream=True)
-            elif action_type in ('content', 'text'):
-                logging.debug('retrieving content of queue id %s', queue_id)
-                response = requests.get(
-                    url,
-                    headers={"User-agent": self.user_agent},
-                    timeout=self.connection_timeout,
-                    stream=False)
-
-            if response.status_code == 200:
-                mime_type = ''
-                content_type = response.headers.get('content-type')
-                if content_type:
-                    mime_type = (content_type).split(';')[0]
-
-                if action_type == 'file':
-                    extension = userprovided.url.determine_file_extension(
-                        url, mime_type)
-                    new_filename = f"{self.file.file_prefix}{queue_id}{extension}"
-                    file_path = self.file.write_response_to_file(
-                        response, new_filename)
-                    hash_value = self.file.get_file_hash(file_path)
-
-                    try:
-                        self.cur.callproc('insert_file_SP',
-                                          (url, url_hash, queue_id, mime_type,
-                                           str(self.file.target_dir),
-                                           new_filename,
-                                           self.file.get_file_size(file_path),
-                                           self.file.hash_method,
-                                           hash_value, 1))
-                    except pymysql.DatabaseError:
-                        self.cnt['transaction_fail'] += 1
-                        logging.error('Transaction failed: Could not add ' +
-                                      'already downloaded file %s to the ' +
-                                      'database!', new_filename)
-
-                elif action_type in ('content', 'text'):
-
-                    detected_encoding = str(response.encoding)
-                    logging.debug('detected encoding: %s', detected_encoding)
-
-                    page_content = response.text
-
-                    if mime_type == 'text/html' and prettify_html:
-                        page_content = self.prettify_html(page_content)
-
-                    if action_type == 'text':
-                        soup = BeautifulSoup(page_content, 'lxml')
-                        page_content = soup.get_text()
-
-                    try:
-                        # Stored procedure saves the content, transfers the
-                        # labels from the queue, and removes the queue item:
-                        self.cur.callproc('insert_content_SP',
-                                          (url, url_hash, queue_id,
-                                           mime_type, page_content, 2))
-                    except pymysql.DatabaseError:
-                        self.cnt['transaction_fail'] += 1
-                        logging.error('Transaction failed: Could not add ' +
-                                      'page code of queue item %s to ' +
-                                      'the database!',
-                                      queue_id, exc_info=True)
-
-                self.stats.increment_processed_counter()
-                self.stats.update_host_statistics(url, 1, 0, 0, 0)
-
-            elif response.status_code in self.HTTP_PERMANENT_ERRORS:
-                self.errorhandling.mark_permanent_error(
-                    queue_id, response.status_code)
-                self.stats.update_host_statistics(url, 0, 0, 1, 0)
-            elif response.status_code == 429:
-                # The server tells explicity that the bot hit a rate limit!
-                logging.error('The bot hit a rate limit! It queries too ' +
-                              'fast => increase min_wait.')
-                fqdn = urlparse(url).hostname
-                if fqdn:
-                    self.errorhandling.add_rate_limit(fqdn)
-                self.stats.update_host_statistics(url, 0, 0, 0, 1)
-            elif response.status_code in self.HTTP_TEMP_ERRORS:
-                logging.info('Temporary error. Adding delay to queue item.')
-                self.errorhandling.add_crawl_delay(
-                    queue_id, response.status_code)
-            else:
-                logging.error('Unhandled return code %s', response.status_code)
-                self.stats.update_host_statistics(url, 0, 0, 1, 0)
-
-        except TimeoutError:
-            logging.error('Reached timeout.', exc_info=True)
-            self.errorhandling.add_crawl_delay(queue_id, 4)
-            self.stats.update_host_statistics(url, 0, 1, 0, 0)
-
-        except ConnectionError:
-            logging.error('Connection Error', exc_info=True)
-            self.stats.update_host_statistics(url, 0, 1, 0, 0)
-            raise
-
-        except urllib3.exceptions.NewConnectionError:
-            logging.error('New Connection Error: might be a rate limit',
-                          exc_info=True)
-            self.stats.update_host_statistics(url, 0, 0, 0, 1)
-            self.time.increase_wait()
-
-        except requests.exceptions.MissingSchema:
-            logging.error('Missing Schema Exception. Does your URL contain ' +
-                          'the protocol i.e. http:// or https:// ? ' +
-                          'See queue_id = %s', queue_id)
-            self.errorhandling.mark_permanent_error(queue_id, 1)
-
-        except Exception:
-            logging.error('Unknown exception while trying to download.',
-                          exc_info=True)
-            self.stats.update_host_statistics(url, 0, 0, 1, 0)
-            raise
+        if action_type == 'file':
+            if prettify_html:
+                logging.error('Wrong action_type: prettify_html ignored.')
+            GetFile(self.objects, queue_id, url, url_hash, False)
+        elif action_type == 'content':
+            GetContent(self.objects, queue_id, url, url_hash, prettify_html)
+        elif action_type == 'text':
+            GetText(self.objects, queue_id, url, url_hash, prettify_html)
+        else:
+            raise ValueError('Invalid action_type!')
 
     def return_page_code(self,
                          url: str) -> str:
-        """Directly return a page's code. Do *not* store it in the database."""
-        if url == '' or url is None:
+        "Directly return a page's code. Do *not* store it in the database."
+        if not url:
             raise ValueError('Missing url')
         url = url.strip()
 
@@ -265,7 +334,6 @@ class ExoActions:
                                self.file.get_file_size(path),
                                self.file.hash_method, hash_value, 3))
         except pymysql.DatabaseError:
-            self.cnt['transaction_fail'] += 1
             logging.error('Database Transaction failed: Could not add ' +
                           'already downloaded file %s to the database!',
                           path, exc_info=True)
