5b251bbd8d204bb43b441a563bbcf7b53936d59c
Rüdiger Voigt <ruedigervoigt@gmx.net>
2021-02-15 23:50:38 +0100

    Split up tests into two groups as some only run on Linux

--- tests.py
+++ tests.py
@@ -4,20 +4,29 @@
 """
 Automatic Tests for exoskeleton
 
-This file contains two kinds of tests:
-* Unit-tests without side-effects that run extremly fast and cover 1/3 to 1/2.
+! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !  ! ! ! ! ! ! ! ! !
+There are two groups of automatic tests for exoskeleton:
+* Unit-tests without side-effects that run extremly fast and cover about 1/3.
 * A system-test that actually interacts with a database and the network.
 
-While the latter one is much slower, it also tests the SQL code and the code
+This file contains the latter group of tests.
+! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !  ! ! ! ! ! ! ! ! !
+
+While the system-test is much slower, it also tests the SQL code and the code
 interacting with the network. It simulates a user interacting with the
 framework.
+
 Even though much *could* be mocked, it is better to actually run it:
 There might be changes to the SQL part and sometimes the DBMS itself
 might introduce a bug.
 
-To run these tests here:
-coverage run --source exoskeleton -m pytest tests.py
-To generate a report afterwards.
+This kind of test needs a service container with a running MariaDB instance.
+At this moment (Feb 2021) those are only supported for Linux:
+https://docs.github.com/en/actions/guides/about-service-containers
+
+To run only the tests here:
+coverage run --source exoskeleton -m pytest tests_with_sideffects.py
+To generate a report limited to that run afterwards:
 coverage html
 ~~~~~~~~~~~~~~~~~~~~~
 Source: https://github.com/RuedigerVoigt/exoskeleton
@@ -42,94 +51,6 @@ from exoskeleton import remote_control_chrome
 from exoskeleton import statistics_manager
 from exoskeleton import time_manager
 
-
-# #############################################################################
-# TESTS WITHOUT A FULL INSTANCE
-# #############################################################################
-
-def test_DatabaseConnection():
-    # missing settings dictionary
-    with pytest.raises(ValueError):
-        database_connection.DatabaseConnection(None)
-
-
-def test_FileManager_functions():
-    # empty string
-    assert file_manager.FileManager._FileManager__clean_prefix('') == ''
-    # only whitespace
-    assert file_manager.FileManager._FileManager__clean_prefix('   ') == ''
-    # exactly 16 characters ( = allowed length) plus whitespace
-    assert file_manager.FileManager._FileManager__clean_prefix('   1234567890123456  ') == '1234567890123456'
-    # more than 16 characters ( = allowed length)
-    with pytest.raises(ValueError):
-        assert file_manager.FileManager._FileManager__clean_prefix('12345678901234567') == ''
-    assert file_manager.FileManager._FileManager__clean_prefix(None) == ''
-
-
-def test_Notificationmanager():
-    notification_manager.NotificationManager('test', None, None)
-
-
-def test_RemoteControlChrome():
-    remote_control_chrome.RemoteControlChrome(None, None, None)
-
-
-def test_RemoteControlChrome_functions():
-    my_chrome = remote_control_chrome.RemoteControlChrome(None, None, None)
-    with pytest.raises(ValueError):
-        assert my_chrome.check_browser_support('test') is False
-    with pytest.raises(ValueError):
-        assert my_chrome.check_browser_support('SaFaRi') is False
-    assert my_chrome.check_browser_support('google-chrome') is True
-    # no browser selected but trying to save a page:
-    with pytest.raises(ValueError):
-        my_chrome.page_to_pdf('https://www.example.com', './', '12343454')
-
-
-def test_TimeManager():
-    # No parameters
-    time_manager.TimeManager()
-    # Valid parameters
-    time_manager.TimeManager(10, 40)
-    # non numeric value for wait_min
-    with pytest.raises(ValueError):
-        time_manager.TimeManager('abc', 40)
-    # non numeric value for wait_max
-    with pytest.raises(ValueError):
-        time_manager.TimeManager(10, 'abc')
-    # Contradiction: wait_min > wait_max
-    with pytest.raises(ValueError):
-        time_manager.TimeManager(100, 10)
-
-
-def test_TimeManager_functions():
-    my_tm = time_manager.TimeManager()
-    # Increase the wait time
-    min_before = my_tm.wait_min
-    max_before = my_tm.wait_max
-    my_tm.increase_wait()
-    assert my_tm.wait_min == min_before + 1
-    assert my_tm.wait_max == max_before + 1
-    # process time
-    assert isinstance(my_tm.get_process_time(), float) is True
-    # absolute run time
-    assert isinstance(my_tm.absolute_run_time(), float) is True
-    # estimate for remaining time
-    assert my_tm.estimate_remaining_time(0, 0) == -1  # nothing done
-    assert my_tm.estimate_remaining_time(0, 10000) == -1  # nothing done
-    assert my_tm.estimate_remaining_time(10000, 0) == 0
-    assert isinstance(my_tm.estimate_remaining_time(10000, 10), int) is True
-    # random-wait needs to be patched
-    with patch('time.sleep', return_value=None):
-        my_tm.random_wait()
-
-
-def test_actions():
-    # prettify_html
-    # Not checking how the improved version looks like as this may change
-    # slightly with newer version of beautiful soup.
-    broken_html = "<a href='https://www.example.com'>example</b></b><p></p>"
-    assert actions.ExoActions.prettify_html(broken_html) != broken_html
 
 # #############################################################################
 # TESTS THAT NEED AN INSTANCE OF EXOSKELETON
@@ -804,9 +725,9 @@ assert 'milestone_num must be integer' in str(excinfo.value)
 valid_mail = exoskeleton.Exoskeleton(
     project_name='Exoskeleton Validation Test',
     database_settings={'port': DB_PORT,
-                        'database': 'exoskeleton',
-                        'username': 'exoskeleton',
-                        'passphrase': 'exoskeleton'},
+                       'database': 'exoskeleton',
+                       'username': 'exoskeleton',
+                       'passphrase': 'exoskeleton'},
     filename_prefix='EXO_',
     target_directory='./fileDownloads',
     mail_settings={
